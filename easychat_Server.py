d080c2352c3983f1825782746780731cfb465713
alfred-ghost <75513331+alfred-ghost@users.noreply.github.com>
2022-06-09 09:32:20 +0800

    Update easychat_Server.py

--- easychat_Server.py
+++ easychat_Server.py
@@ -6,21 +6,26 @@ import time
 
 info_box = []
 info_lock = threading.Lock()
-
+filter_list = [b'http',b'Http',b'HTTP',b'cookie',b'Cookie',b'<html>',b'<HTML>']
 def check(string):
+	# 异常访问检测
+	global filter_list
 	if string == None:
 		return True
-	elif b"http" in string or b'Http' in string or b'HTTP' in string:
-		return False
 	else:
+		for i in filter_list:
+			if i in string:
+				return False
 		return True 
 
 def log(info):
-	with open("log.txt",'a',encoding='utf-8') as f:
-		f.write("\n"+info)
+	# 记录日志
+	with open(r"X:\阿里云盘\log\log.txt",'a',encoding='utf-8') as f:
+		f.write("\n"+info+'\t'+time.asctime(time.localtime()))
 
 def read_client(s,addr):
-		global socket_list,socket_lock
+		# 读取请求连接的客户端
+		global socket_list,socket_lock,socket_ip_list
 		try:
 			content = s.recv(1024)
 			return content
@@ -28,34 +33,49 @@ def read_client(s,addr):
 			print("%s 取消连接"%str(addr))
 			log("\ninfo: %addr cancel connection"%str(addr))
 			socket_lock.acquire()
-			if s in socket_list:
+			try:
 				socket_list.remove(s)
 				socket_ip_list.remove(addr[0])
+			except:
+				pass
 			socket_lock.release()
 			return None
 
 def socket_target(s,addr):
-	global socket_list,socket_lock
+	# 广播函数
+	global socket_list,socket_lock,socket_ip_list
 	print("%s 连接"%str(addr))
 	log("\ninfo: %addr build connection"%str(addr))
-	try:
+	#try:
+	if 1:
 		while(1):
 			content = read_client(s,addr)
 			print(content)
+			log(str(content)+ "\tfrom\t"+str(addr))
 			info_lock.acquire()
 			info_box.append((s,addr,content))
 			info_lock.release()
 			if content is None:
 				break
 			else:
+				socket_lock.acquire()
+				temp = []
 				if check(content):
-					for i in socket_list:
+					# 检测失败的内容不广播
+					for k,i in enumerate(socket_list):
 						if i != s:
-							i.send(content)
-	except:
-		log("\nerror: an error occur where socket_target")
+							try:
+								i.send(content)
+							except:
+								temp.append([i,socket_ip_list[k]])
+				for i,j in temp:
+					socket_list.remove(i)
+					socket_ip_list.remove(j)
+				socket_lock.release()
+				time.sleep(0.5)		
 
 def robot():
+	# 机器人，用于在无人时回复简单消息，并对检测的ip进行封禁
 	global socket_list,socket_lock,info_box,info_lock,socket_ip_list
 	history = None
 	while(1):
@@ -64,26 +84,28 @@ def robot():
 		socket_lock.acquire()
 		if len(socket_list) == 1:
 			if socket_list[0] != history:
+				# 没人在这！
 				socket_list[0].send("robot says: nobody here!".encode())
 				history = socket_list[0]
 		socket_lock.release()
 		info_lock.acquire()
 		for i in info_box:
+			# 对检测成功的ip连接进行关闭
 			if not check(i[2]):
 				ip , _ = i[1]
-				#socket_lock.acquire()
+				socket_lock.acquire()
 				for k in range(len(socket_list)):
 					if ip == socket_ip_list[k]:
 						socket_list[k].close()
-						socket_lock.acquire()
-						socket_list.pop(k)
-						socket_ip_list.pop(k)
-						socket_lock.release()
-				#socket_lock.release()
+				socket_lock.release()
 				with open("ip.txt",'a',encoding='utf-8') as f:
+					# 记录Ip封禁日志
 					f.write (str(i[1]).split(',')[0][2:-1]+'\n')
 		info_box.clear()
 		info_lock.release()
+		with open("ip_state.txt",'w',encoding='utf-8') as f:
+			for i in socket_ip_list:
+				f.write(str(i)+'\n')
 		time.sleep(1)
 			
 
@@ -95,14 +117,16 @@ if __name__ == "__main__":
 	s = socket()
 	s.bind(("",10000))
 	s.listen(10)
+	# 最大接收10个用户可持续连接
 	threading.Thread(target = robot).start()
-	while(1):
+	while(1): # 循环接收用户连接并建立线程
 		conn,addr = s.accept()
 		ip_list = []
 		with open('ip.txt','r',encoding='utf-8') as f:
 			ip_list =  f.read().split('\n')
 		ip,_ = addr
 		if ip not in ip_list:
+			# ip_list 是被封禁的Ip
 			socket_lock.acquire()
 			socket_list.append(conn)
 			socket_ip_list.append(addr[0])
@@ -111,7 +135,10 @@ if __name__ == "__main__":
 			threading.Thread(target = socket_target,args=(conn,addr)).start()
 		else:
 			print("refuse the request by %s"%(str(addr)))
-			conn.send('you are refused to access to server'.encode())
-			
+			try:
+				conn.send('you are refused to access to server'.encode())
+				conn.close()
+			except:
+				continue		
 		
 		
